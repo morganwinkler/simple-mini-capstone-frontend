353e7db824cad19f2e2f79cda227840cc422a3fa
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-11 10:03:00 -0500

    Map through array in props

--- src/PhotosIndex.jsx
+++ src/PhotosIndex.jsx
@@ -1,7 +1,19 @@
-export function PhotosIndex() {
+/* eslint-disable react/prop-types */
+
+// tells the child component that it will be taking in props as an argument
+export function PhotosIndex(props) {
+
     return (
       <div>
+
         <h1>All photos</h1>
+
+        {props.photos.map((photo) => (
+            <div key={photo.id}>
+                <h2>{photo.name}</h2>
+                <img src={photo.url} alt="" />
+            </div>
+        ))}
       </div>
     );
   }
