0171622511a6110d1035cbcc304d745f4ac759d1
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-12 16:01:37 -0500

    Style header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,30 @@
 export function Header() {
     return (
       <header>
-        <nav>
-          <a href="#">Home</a> | <a href="#">Link</a> | <a href="#">Link</a>
+        <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Morgan&#39;s Box of Treasures</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">New Product</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" aria-disabled="true">All Products</a>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </nav>
       </header>
     )
