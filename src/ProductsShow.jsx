7e1383706e81df928cfda6d8be978bf2d82041ac
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-12 15:30:14 -0500

    Add edit form w/default values to modal

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -5,6 +5,22 @@ export function ProductsShow(props) {
         <h1>Product Information</h1>
         <p>Name: {props.product.name}</p>
         <p>Description: {props.product.description}</p>
+        <form action="">
+          <h1>Would you like to update {props.product.name}?</h1>
+          <div>
+            Name: <input defaultValue={props.product.name} name="name" type="text" />
+          </div>
+          <div>
+            Description: <input defaultValue={props.product.description} name="description" type="text" />
+          </div>
+          <div>
+            Price: <input defaultValue={props.product.price} name="price" type="text" />
+          </div>
+          <div>
+            Image Url: <input defaultValue={props.product.image_url} name="image_url" type="text" />
+          </div>
+          <button type="submit"> Update Item</button>
+        </form>
       </div>
     );
   }
