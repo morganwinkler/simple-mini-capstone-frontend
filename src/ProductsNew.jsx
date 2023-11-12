e766638edf9c3c43d82bef6e90dac7dbf049cd6e
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-12 16:24:13 -0500

    Style new form

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -10,23 +10,31 @@ export function ProductsNew(props) {
          };
 
     return (
-      <div>
-        <h1>New Product</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Name: <input name="name" type="text" />
-          </div>
-          <div>
-            Description: <input name="description" type="text" />
-          </div>
-          <div>
-            Price: <input name="price" type="text" />
-          </div>
-          <div>
-            Image URL: <input name="image_url" type="text" />
-          </div>
-          <button type="submit">Create Product</button>
-        </form>
+      <div className="mb-3 card" style={{paddingTop: "15px",paddingBottom: "15px", marginTop: "50px"}}>
+        <h1 className="text-center">New Product</h1>
+        <div className="mb-3">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3" style={{padding: "15px"}}>
+              <label className="form-label">Name</label>
+              <input className="form-control" name="name" type="text" />
+            </div>
+            <div className="mb-3" style={{padding: "15px"}}>
+              <label className="form-label">Description</label>
+              <input className="form-control" name="description" type="text" />
+            </div>
+            <div className="mb-3" style={{padding: "15px"}}>
+              <label className="form-label">Price</label>
+              <input className="form-control" name="price" type="text" />
+            </div>
+            <div className="mb-3" style={{padding: "15px"}}>
+              <label className="form-label">Image URL</label>
+              <input className="form-control" name="image_url" type="text" />
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary">Create Product</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
