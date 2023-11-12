064afcd9db0beaf83634ad51383e82b39a815f07
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-12 15:47:33 -0500

    Add bootstrap
    "npm install bootstrap --save" from inside frontend folder

--- src/Content.jsx
+++ src/Content.jsx
@@ -66,7 +66,7 @@ export function Content() {
   useEffect(handleIndexProducts, []);
 
     return (
-      <div>
+      <div className="container">
         <ProductsNew onCreateProduct={handleCreateProduct} />
         <ProductsIndex products={products} onShowProduct={handleShowProduct} />
         <Modal show={isProductsShowVisible} onClose={handleClose}>
