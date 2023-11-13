53273e44adfb8ff4964ccd69d5975f845ef4ecf2
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-13 10:14:06 -0500

    Style all products

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -5,18 +5,24 @@ export function ProductsIndex(props) {
 
     return (
       <div>
-
-        <h1>All Products</h1>
-
-        {props.products.map((product) => (
-            <div key={product.id}>
-                <h2>{product.name}</h2>
-                <p>{product.description}</p>
-                <p>{product.price}</p>
-                <img src={product.image_url} alt="" />
-                <button onClick={() => props.onShowProduct(product)}>More Info</button>
-            </div>
-        ))}
+        <div className="row text-center">
+          <h1 style={{margin: "25px"}}>All Products</h1>
+        </div>
+          {/* justify-content-md-center to center items within the row */}
+          <div className="row justify-content-md-center">
+            {props.products.map((product) => (
+                <div key={product.id} className="card text-bg-light" style={{width: "18rem", margin: "5px"}}>
+                  <img className="card-img-top"  src={product.image_url} alt="" style={{maxHeight: "250px"}} />
+                  <div className="card-body">
+                    <h2 className="card-title">{product.name}</h2>
+                    <p className="card-text">{product.description}</p>
+                    <p className="card-text">{product.price}</p>
+                  </div>
+                    <button className="btn btn-primary" onClick={() => props.onShowProduct(product)}>More Info</button>
+                </div>
+            ))}
+          </div>
+       
       </div>
     );
   }
