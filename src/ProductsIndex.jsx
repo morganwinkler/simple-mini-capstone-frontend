01f858a2d35ca140d0631f5417c554e697cc3e2d
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-14 11:32:48 -0500

    Add annotations

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
 
-
-export function ProductsIndex(props) {
-
+// tells the function that it will be taking in props from the parent component
+export function ProductsIndex(props) {                                                                                        
     return (
       <div>
         <div className="row text-center">
@@ -10,7 +9,9 @@ export function ProductsIndex(props) {
         </div>
           {/* justify-content-md-center to center items within the row */}
           <div className="row justify-content-md-center">
+            {/* takes the props and goes through each item in the prop element */}
             {props.products.map((product) => (
+              // key=product.id is needed for the map
                 <div key={product.id} className="card text-bg-light" style={{width: "18rem", margin: "5px"}}>
                   <img className="card-img-top"  src={product.image_url} alt="" style={{maxHeight: "250px"}} />
                   <div className="card-body">
@@ -18,6 +19,7 @@ export function ProductsIndex(props) {
                     <p className="card-text">{product.description}</p>
                     <p className="card-text">{product.price}</p>
                   </div>
+                    {/* on the mouse click event we want to take the props of onShowProduct from the parent component with the argument of product */}
                     <button className="btn btn-primary" onClick={() => props.onShowProduct(product)}>More Info</button>
                 </div>
             ))}
--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
+
+// tells the modal that it will be taking in props from the parent element
 export function ProductsShow(props) {
 
+  // creates a function that takes in event as an argument
   const handleSubmit = (event) => {
+        // by default the browser will want to send the form data to the current URL and refresh the page - this prevents that
          event.preventDefault();
+        //  establishes that the parameters are coming from the form that is below
+        // event.target refers to the HTML element that triggered the event - in this case is a button click
          const params = new FormData(event.target);
+        //  calls the props parent function onUpdateProduct on the product with the given id - passes params to it, clears form on button click
          props.onUpdateProduct(props.product.id, params, () => event.target.reset());
        }; 
+
+  // creates a function that will destroy a product by calling on the parent function onDestroyProduct and passing in the product
   const handleClick = () => {
         props.onDestroyProduct(props.product);
        };
@@ -15,6 +24,7 @@ export function ProductsShow(props) {
         <h1>Product Information</h1>
         <p>Name: {props.product.name}</p>
         <p>Description: {props.product.description}</p>
+        {/* onSubmit = form handler function that fires when form gets submitted */}
         <form onSubmit={handleSubmit}>
           <h1>Would you like to update {props.product.name}?</h1>
           <div>
@@ -31,6 +41,7 @@ export function ProductsShow(props) {
           </div>
           <button type="submit"> Update Item</button>
         </form>
+        {/* onClick = mouse event handler function - fires when primary button was clicked on the pointing device */}
         <button onClick={handleClick}>Delete Item</button>
       </div>
     );
